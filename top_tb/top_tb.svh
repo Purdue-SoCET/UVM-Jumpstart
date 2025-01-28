6c20d1ab41766029e2bff53138631bda3030f75e
Furkan Y. Akyildiz <fyabu@bu.edu>
2025-01-28 17:04:51 -0500

    top testbench is created

---
+++ top_tb/top_tb.svh
@@ -0,0 +1,20 @@
+module top_tb;
+
+   initial
+   begin
+      // Initialize reset
+   end
+
+   // Clock Generation
+
+
+   // Interface declarations
+
+   // Connection of DUT
+   initial
+   begin
+      // Register interfaces to uvm_config_db
+      // Run UVM test class
+   end
+
+endmodule: top_tb
