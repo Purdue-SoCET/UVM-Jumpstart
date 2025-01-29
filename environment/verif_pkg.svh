b2f86bd047141edfc2760f2fead55495ce6aceeb
sudohdl <fyabu@bu.edu>
2025-01-30 01:53:39 +0300

    environment and test classes are compiled properly

--- environment/verif_pkg.svh
+++ environment/verif_pkg.svh
@@ -5,7 +5,7 @@ package verif_pkg;
 
     `include "uvm_macros.svh"
  
-    `include "../env/env_top.svh"
+    `include "../environment/env_top.svh"
 
     `include "../test/test_top.svh"
     `include "../test/tc_direct_urx.svh"
