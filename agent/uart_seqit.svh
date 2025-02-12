87cceef882a73728ddb5de331e1cd5d0917a2b8c
sudohdl <fyabu@bu.edu>
2025-02-12 15:33:05 -0500

    expected data is getting filled from test, but timing needs to be adjusted

--- agent/uart_seqit.svh
+++ agent/uart_seqit.svh
@@ -6,9 +6,16 @@ class uart_seqit extends uvm_sequence_item;
     bit [7:0] mon_data;
 
     extern function new(string name="uart_seqit");
+    extern function void do_print(uvm_printer printer);
 
 endclass:uart_seqit
 
 function uart_seqit::new(string name="uart_seqit");
     super.new(name);
 endfunction:new
+
+function void uart_seqit::do_print(uvm_printer printer);
+    super.do_print(printer);
+    printer.print_field_int("mon_data", mon_data, $bits(mon_data), UVM_HEX);
+    // printer.print_field_int("data_arr", data_arr, $bits(data_arr), UVM_HEX);
+endfunction:do_print
--- test/tc_direct_urx.svh
+++ test/tc_direct_urx.svh
@@ -5,7 +5,7 @@ class tc_direct_urx extends test_top;
 
    extern function new(string name = "tc_direct_urx" , uvm_component parent);
    extern function void build_phase(uvm_phase phase);
-   extern function void connect_phase(uvm_phase phase);
+   extern function void start_of_simulation_phase  (uvm_phase phase);
    extern task run_phase(uvm_phase phase);
 
 endclass:tc_direct_urx
@@ -20,16 +20,19 @@ function void tc_direct_urx::build_phase(uvm_phase phase);
    uart_sequence_h = uart_sequence::type_id::create("uart_sequence_h");
 endfunction:build_phase
 
-function void tc_direct_urx::connect_phase(uvm_phase phase);
+function void tc_direct_urx::start_of_simulation_phase(uvm_phase phase);
    env_h.uart_scoreboard_h.expected_data.push_back(uart_sequence_h.req);
-endfunction:connect_phase
+endfunction:start_of_simulation_phase
 
 task tc_direct_urx::run_phase(uvm_phase phase);
    phase.raise_objection(this);
+   `uvm_info(get_type_name(), $sformatf(" XOXOXOX -1 bit : %0d ",env_h.uart_scoreboard_h.expected_data[0].data_arr.size()), UVM_LOW)
    // #1ms;
    `uvm_info("tc_direct_urx", "Hello from run_phase", UVM_LOW)
    // Start seqeuncer
    uart_sequence_h.start(env_h.uart_agent_h.sequencer_h);
+   `uvm_info(get_type_name(), $sformatf(" XOXOXOX -2 bit : %0d ",env_h.uart_scoreboard_h.expected_data.size()), UVM_LOW)
+   `uvm_info(get_type_name(), $sformatf(" XOXOXOX -3 bit : %0d ",uart_sequence_h.req.data_arr.size()), UVM_LOW)
    #100us;
    `uvm_info("tc_direct_urx", "end run_phase", UVM_LOW)
    phase.drop_objection(this);
