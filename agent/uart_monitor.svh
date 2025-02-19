4625ab7c124d843c682317f08a8356bdff311710
sudohdl <fyabu@bu.edu>
2025-02-19 14:21:13 -0500

    monitor is updated

--- agent/uart_monitor.svh
+++ agent/uart_monitor.svh
@@ -7,6 +7,7 @@ class uart_monitor extends uvm_monitor;
    // Virtual interface    
    virtual uart_if vif;
    real bit_time;
+   bit parity;
 
    // The monitored data written to monitor_port
   uvm_analysis_port #(uart_seqit) monitor_port;
@@ -47,38 +48,82 @@ task uart_monitor::collect_data();
    forever begin
       uart_seqit mon_seqit;
       mon_seqit = uart_seqit::type_id::create("mon_seqit");
+      parity = 0;
 
       // Detect start bit
       wait(!vif.tx_dout_o);
       // Wait until middle of start bit
       #(bit_time/2);
-
       if(vif.tx_dout_o == 0) begin
          START_BIT_PASSED: `uvm_info(get_type_name(), "Start bit is detected ", UVM_LOW)
       end else begin
          START_BIT_FAILED: `uvm_error(get_type_name(), "Start Bit is not detected")
       end
-
-      // Calculate parity with data from bus
+      // Sample data and calculate parity bit
       for(int i = 0; i < uart_config_h.number_data_bits; i++) begin
          #(bit_time);
          mon_seqit.mon_data[i] <= vif.tx_dout_o;
+         parity ^= vif.tx_dout_o;
       end
-
-      // Go to the middle of first stop bit
-      #(bit_time);
-      if(vif.tx_dout_o == 1'b1) begin
-         STOP_BIT_PASSED_FIRST: `uvm_info(get_type_name(), "First stop bit is detected", UVM_LOW)
-      end else begin
-         STOP_BIT_FAILED_FIRST: `uvm_error(get_type_name(), "First Stop bit is not detected")
-      end
-      // Go to the middle of second stop bit
-      #(bit_time);
-      if(vif.tx_dout_o == 1'b1) begin
-         STOP_BIT_PASSED_SECOND: `uvm_info(get_type_name(), "Second stop bit is detected", UVM_LOW)
-      end else begin
-         STOP_BIT_FAILED_SECOND: `uvm_error(get_type_name(), "Second stop bit is not detected")
-      end
+      // Parity check
+      case (uart_config_h.parity_bit)
+         PARITY_EVEN : begin
+            #(bit_time);
+            parity = parity;
+            // Compare parity bits
+            if(vif.tx_dout_o == parity) begin
+               PARITY_EVEN_DETECTED: `uvm_info(get_type_name(), "Parity bit is correct", UVM_LOW)
+            end 
+            else begin
+               PARITY_EVEN_ERROR: `uvm_error(get_type_name(), "Parity error detected")
+            end
+         end
+         PARITY_ODD  : begin
+            #(bit_time);
+            parity = ~parity;
+            // Compare parity bits
+            if(vif.tx_dout_o == parity) begin
+               PARITY_ODD_DETECTED: `uvm_info(get_type_name(), "Parity bit is correct", UVM_LOW)
+            end 
+            else begin
+               PARITY_ODD_ERROR: `uvm_error(get_type_name(), "Parity error detected")
+            end
+         end
+         PARITY_NONE : begin
+            PARITY_NONE_DETECTED: `uvm_info(get_type_name(), "Parity none is set", UVM_LOW)
+         end
+      endcase
+      // Stop bit check
+      case (uart_config_h.stop_bit)
+         STOP_BIT_TWOBITS  : begin
+            // Go to the middle of first stop bit
+            #(bit_time);
+            if(vif.tx_dout_o == 1'b1) begin
+               STOP_BIT_PASSED_FIRST: `uvm_info(get_type_name(), "First stop bit is detected", UVM_LOW)
+            end 
+            else begin
+               STOP_BIT_FAILED_FIRST: `uvm_error(get_type_name(), "First Stop bit is not detected")
+            end
+            // Go to the middle of second stop bit
+            #(bit_time);
+            if(vif.tx_dout_o == 1'b1) begin
+               STOP_BIT_PASSED_SECOND: `uvm_info(get_type_name(), "Second stop bit is detected", UVM_LOW)
+            end 
+            else begin
+               STOP_BIT_FAILED_SECOND: `uvm_error(get_type_name(), "Second stop bit is not detected")
+            end
+         end
+         STOP_BIT_ONEBIT   : begin
+            // Go to the middle of stop bit
+            #(bit_time);
+            if(vif.tx_dout_o == 1'b1) begin
+               STOP_BIT_PASSED: `uvm_info(get_type_name(), "Stop bit is detected", UVM_LOW)
+            end 
+            else begin
+               STOP_BIT_FAILED: `uvm_error(get_type_name(), "Stop bit is not detected")
+            end
+         end
+      endcase
       monitor_port.write(mon_seqit);
    end
 
